feb54cf07d31c9b57e6453793b9dbb69d077c6a8
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-02-25 14:50:57 -0500

    Blockstates fixed!

--- src/main/java/com/direwolf20/justdirethings/datagen/BlockStates.java
+++ src/main/java/com/direwolf20/justdirethings/datagen/BlockStates.java
@@ -3,9 +3,12 @@ package com.direwolf20.justdirethings.datagen;
 import com.direwolf20.justdirethings.JustDireThings;
 import com.direwolf20.justdirethings.common.blocks.gooblocks.GooPatternBlock;
 import com.direwolf20.justdirethings.setup.Registration;
+import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.client.model.generators.ConfiguredModel;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 import java.util.Objects;
@@ -20,9 +23,57 @@ public class BlockStates extends BlockStateProvider {
     protected void registerStatesAndModels() {
         simpleBlock(Registration.GooBlock_Tier1.get(), models().cubeAll(Registration.GooBlock_Tier1_ITEM.getId().getPath(), blockTexture(Registration.GooBlock_Tier1.get())));
         simpleBlock(Registration.DireIronBlock.get(), models().cubeAll(Registration.DireIronBlock_ITEM.getId().getPath(), blockTexture(Registration.DireIronBlock.get())));
+        patternBlock();
+    }
+
+    private void patternBlock() {
+        getVariantBuilder(Registration.GooPatternBlock.get()).forAllStates(s -> {
+            int stage = s.getValue(GooPatternBlock.GOOSTAGE);
+            Direction dir = s.getValue(BlockStateProperties.FACING);
+            ModelFile model;
+            if (stage == 11) {
+                model = models().orientableWithBottom(
+                        Objects.requireNonNull(Registration.GooPatternBlock.getId()).getPath() + "11",
+                        modLoc("block/goopatterns/goorender_full"),
+                        modLoc("block/goopatterns/goorender_full"),
+                        modLoc("block/goopatterns/goorender_full"),
+                        modLoc("block/goopatterns/goorender_full")
+                ).renderType("cutout");
+            } else if (stage == 10) {
+                model = models().orientableWithBottom(
+                        Objects.requireNonNull(Registration.GooPatternBlock.getId()).getPath() + "10",
+                        modLoc("block/goopatterns/goorender_full"),
+                        modLoc("block/goopatterns/goorender_full"),
+                        modLoc("block/goopatterns/goorender_full"),
+                        modLoc("block/goopatterns/goorender_full")
+                ).renderType("cutout");
+            } else if (stage == 9) {
+                model = models().orientableWithBottom(
+                        Objects.requireNonNull(Registration.GooPatternBlock.getId()).getPath() + "9",
+                        modLoc("block/goopatterns/goorender_full"),
+                        modLoc("block/goopatterns/goorender_full"),
+                        modLoc("block/goopatterns/goorender_full"),
+                        modLoc("block/goopatterns/goopatterblock_top")
+                ).renderType("cutout");
+            } else {
+                model = models().orientableWithBottom(
+                        Objects.requireNonNull(Registration.GooPatternBlock.getId()).getPath() + stage,
+                        modLoc("block/goopatterns/goorender_side" + stage),
+                        modLoc("block/goopatterns/goorender_side" + stage),
+                        modLoc("block/goopatterns/goorender_full"),
+                        modLoc("block/goopatterns/goorender_blank")
+                ).renderType("cutout");
+            }
+            return ConfiguredModel.builder()
+                    .modelFile(model)
+                    .rotationX(dir == Direction.DOWN ? 180 : dir.getAxis().isHorizontal() ? 90 : 0)
+                    .rotationY(dir.getAxis().isVertical() ? 0 : (int) (dir.toYRot() + 180) % 360).build();
+        });
+    }
 
 
-        getVariantBuilder(Registration.GooPatternBlock.get()) //TODO Loop
+
+        /*getVariantBuilder(Registration.GooPatternBlock.get()) //TODO Loop
                 .partialState().with(GooPatternBlock.GOOSTAGE, 0).setModels(new ConfiguredModel(models().orientableWithBottom(
                         Objects.requireNonNull(Registration.GooPatternBlock.getId()).getPath() + "0",
                         modLoc("block/goopatterns/goorender_side0"),
@@ -107,7 +158,7 @@ public class BlockStates extends BlockStateProvider {
                         modLoc("block/goopatterns/goorender_full"),
                         modLoc("block/goopatterns/goorender_full")
                 ).renderType("cutout")))
-        ;
+        ;*/
     /*directionalBlock(Registration.GooPatternBlock.get(), models().orientableWithBottom(
                 Objects.requireNonNull(Registration.GooPatternBlock.getId()).getPath(),
                 modLoc("block/goopatterblock_side"),
@@ -115,6 +166,5 @@ public class BlockStates extends BlockStateProvider {
                 modLoc("block/goopatterblock_bottom"),
                 modLoc("block/goopatterblock_top")
         ).renderType("cutout")));*/
-    }
 
 }
