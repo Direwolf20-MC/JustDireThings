f1d4832ff36c118cd0c071f71d82383fa999a29b
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-03-03 12:50:08 -0500

    Fix: Don't Farmland ALL THE THINGS

--- src/main/java/com/direwolf20/justdirethings/common/events/BlockToolModification.java
+++ src/main/java/com/direwolf20/justdirethings/common/events/BlockToolModification.java
@@ -2,15 +2,20 @@ package com.direwolf20.justdirethings.common.events;
 
 import com.direwolf20.justdirethings.common.items.tools.FerricoreHoe;
 import com.direwolf20.justdirethings.setup.Registration;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.common.ToolActions;
 import net.neoforged.neoforge.event.level.BlockEvent;
 
 public class BlockToolModification {
 
     @SubscribeEvent
     public static void handleTickEndEvent(BlockEvent.BlockToolModificationEvent event) {
-        if (event.getHeldItemStack().getItem() instanceof FerricoreHoe) {
-            event.setFinalState(Registration.GooSoil.get().defaultBlockState());
+        if (event.getToolAction().equals(ToolActions.HOE_TILL) && event.getHeldItemStack().getItem() instanceof FerricoreHoe) {
+            BlockState modifiedState = event.getState().getBlock().getToolModifiedState(event.getState(), event.getContext(), event.getToolAction(), event.isSimulated());
+            if (modifiedState != null && modifiedState.is(Blocks.FARMLAND))
+                event.setFinalState(Registration.GooSoil.get().defaultBlockState());
         }
     }
 }
