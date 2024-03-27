16cdf1fd62fabbc3b1247e2e6407f8de77757ebb
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-03-27 13:55:19 -0400

    VoxelShape for Bounding box - WIP

---
+++ src/main/java/com/direwolf20/justdirethings/common/blocks/baseblocks/BaseRawOre.java
@@ -0,0 +1,93 @@
+package com.direwolf20.justdirethings.common.blocks.baseblocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import java.util.stream.Stream;
+
+public class BaseRawOre extends Block {
+    protected static final VoxelShape[] shapes = new VoxelShape[]{
+            Stream.of(
+                    Block.box(8, 8, 8, 12, 14, 12),
+                    Block.box(2, 10, 2, 8, 16, 8),
+                    Block.box(4, 14, 4, 14, 16, 14),
+                    Block.box(6, 2, 6, 10, 14, 10),
+                    Block.box(8, 12, 2, 12, 16, 6),
+                    Block.box(4, 6, 4, 8, 10, 8)
+            ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get(), //DOWN
+            Stream.of(
+                    Block.box(8, 2, 8, 12, 8, 12),
+                    Block.box(2, 0, 2, 8, 6, 8),
+                    Block.box(4, 0, 4, 14, 2, 14),
+                    Block.box(6, 2, 6, 10, 14, 10),
+                    Block.box(8, 0, 2, 12, 4, 6),
+                    Block.box(4, 6, 4, 8, 10, 8)
+            ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get(), //UP
+            Stream.of(
+                    Block.box(8, 4, 8, 12, 8, 14),
+                    Block.box(2, 8, 10, 8, 14, 16),
+                    Block.box(4, 2, 14, 14, 12, 16),
+                    Block.box(6, 6, 2, 10, 10, 14),
+                    Block.box(8, 10, 12, 12, 14, 16),
+                    Block.box(4, 8, 6, 8, 12, 10)
+            ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get(), //NORTH
+            Stream.of(
+                    Block.box(8, 4, 2, 12, 8, 8),
+                    Block.box(2, 8, 0, 8, 14, 6),
+                    Block.box(4, 2, 0, 14, 12, 2),
+                    Block.box(6, 6, 2, 10, 10, 14),
+                    Block.box(8, 10, 0, 12, 14, 4),
+                    Block.box(4, 8, 6, 8, 12, 10)
+            ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get(), //SOUTH
+            Stream.of(
+                    Block.box(8, 4, 8, 14, 8, 12),
+                    Block.box(10, 8, 2, 16, 14, 8),
+                    Block.box(14, 2, 4, 16, 12, 14),
+                    Block.box(2, 6, 6, 14, 10, 10),
+                    Block.box(12, 10, 8, 16, 14, 12),
+                    Block.box(6, 8, 4, 10, 12, 8)
+            ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get(), //WEST
+            Stream.of(
+                    Block.box(2, 4, 8, 8, 8, 12),
+                    Block.box(0, 8, 2, 6, 14, 8),
+                    Block.box(0, 2, 4, 2, 12, 14),
+                    Block.box(2, 6, 6, 14, 10, 10),
+                    Block.box(0, 10, 8, 4, 14, 12),
+                    Block.box(6, 8, 4, 10, 12, 8)
+            ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get()//EAST
+    };
+
+    public BaseRawOre(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(BlockStateProperties.FACING, context.getClickedFace());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(BlockStateProperties.FACING);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+        return shapes[state.getValue(BlockStateProperties.FACING).get3DDataValue()];
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getOcclusionShape(BlockState state, BlockGetter reader, BlockPos pos) {
+        return shapes[state.getValue(BlockStateProperties.FACING).get3DDataValue()];
+    }
+}
--- src/main/java/com/direwolf20/justdirethings/common/blocks/resources/RawCelestigemOre.java
+++ src/main/java/com/direwolf20/justdirethings/common/blocks/resources/RawCelestigemOre.java
@@ -1,13 +1,9 @@
 package com.direwolf20.justdirethings.common.blocks.resources;
 
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.block.Block;
+import com.direwolf20.justdirethings.common.blocks.baseblocks.BaseRawOre;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
-public class RawCelestigemOre extends Block {
+public class RawCelestigemOre extends BaseRawOre {
     public RawCelestigemOre() {
         super(Properties.of()
                 .sound(SoundType.AMETHYST)
@@ -15,15 +11,5 @@ public class RawCelestigemOre extends Block {
                 .noOcclusion()
                 .strength(5.0F, 6.0F)
         );
-    }
-
-    @Override
-    public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(BlockStateProperties.FACING, context.getClickedFace());
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(BlockStateProperties.FACING);
     }
 }
