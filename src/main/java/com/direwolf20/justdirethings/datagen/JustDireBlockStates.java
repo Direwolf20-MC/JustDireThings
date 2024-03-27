e698ca68b5d0780fcc669f0d841c97b644403c4a
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-03-27 11:57:10 -0400

    Adjustments for new textures, and prep for new Generators

--- src/main/java/com/direwolf20/justdirethings/datagen/JustDireBlockStates.java
+++ src/main/java/com/direwolf20/justdirethings/datagen/JustDireBlockStates.java
@@ -26,13 +26,13 @@ public class JustDireBlockStates extends BlockStateProvider {
         simpleBlock(Registration.GooBlock_Tier3.get(), models().cubeAll(Registration.GooBlock_Tier3_ITEM.getId().getPath(), blockTexture(Registration.GooBlock_Tier3.get())).renderType("cutout"));
         simpleBlock(Registration.GooBlock_Tier4.get(), models().cubeAll(Registration.GooBlock_Tier4_ITEM.getId().getPath(), blockTexture(Registration.GooBlock_Tier4.get())).renderType("cutout"));
         simpleBlock(Registration.FerricoreBlock.get(), models().cubeAll(Registration.FerricoreBlock_ITEM.getId().getPath(), blockTexture(Registration.FerricoreBlock.get())));
-        simpleBlock(Registration.RawFerricoreOre.get(), models().cubeAll(Registration.RawFerricoreOre_ITEM.getId().getPath(), blockTexture(Registration.RawFerricoreOre.get())).renderType("translucent"));
+        //simpleBlock(Registration.RawFerricoreOre.get(), models().cubeAll(Registration.RawFerricoreOre_ITEM.getId().getPath(), blockTexture(Registration.RawFerricoreOre.get())).renderType("translucent"));
         simpleBlock(Registration.BlazeGoldBlock.get(), models().cubeAll(Registration.BlazeGoldBlock_ITEM.getId().getPath(), blockTexture(Registration.BlazeGoldBlock.get())));
-        simpleBlock(Registration.RawBlazegoldOre.get(), models().cubeAll(Registration.RawBlazegoldOre_ITEM.getId().getPath(), blockTexture(Registration.RawBlazegoldOre.get())).renderType("translucent"));
+        //simpleBlock(Registration.RawBlazegoldOre.get(), models().cubeAll(Registration.RawBlazegoldOre_ITEM.getId().getPath(), blockTexture(Registration.RawBlazegoldOre.get())).renderType("translucent"));
         simpleBlock(Registration.CelestigemBlock.get(), models().cubeAll(Registration.CelestigemBlock_ITEM.getId().getPath(), blockTexture(Registration.CelestigemBlock.get())));
-        simpleBlock(Registration.RawCelestigemOre.get(), models().cubeAll(Registration.RawCelestigemOre_ITEM.getId().getPath(), blockTexture(Registration.RawCelestigemOre.get())).renderType("translucent"));
+        //simpleBlock(Registration.RawCelestigemOre.get(), models().cubeAll(Registration.RawCelestigemOre_ITEM.getId().getPath(), blockTexture(Registration.RawCelestigemOre.get())).renderType("translucent"));
         simpleBlock(Registration.EclipseAlloyBlock.get(), models().cubeAll(Registration.EclipseAlloyBlock_ITEM.getId().getPath(), blockTexture(Registration.EclipseAlloyBlock.get())));
-        simpleBlock(Registration.RawEclipseAlloyOre.get(), models().cubeAll(Registration.RawEclipseAlloyOre_ITEM.getId().getPath(), blockTexture(Registration.RawEclipseAlloyOre.get())).renderType("translucent"));
+        //simpleBlock(Registration.RawEclipseAlloyOre.get(), models().cubeAll(Registration.RawEclipseAlloyOre_ITEM.getId().getPath(), blockTexture(Registration.RawEclipseAlloyOre.get())).renderType("translucent"));
 
         simpleBlock(Registration.BlockBreakerT2.get(), models().cubeAll(Registration.BlockBreakerT2_ITEM.getId().getPath(), blockTexture(Registration.BlockBreakerT2.get())));
 
