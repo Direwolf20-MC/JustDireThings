bd8d4aedebcf3dbe8bc8608dc5eaba9704526fed
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-04-05 23:42:48 -0400

    Prep for Rid Textures

--- src/main/java/com/direwolf20/justdirethings/datagen/JustDireBlockStates.java
+++ src/main/java/com/direwolf20/justdirethings/datagen/JustDireBlockStates.java
@@ -36,10 +36,10 @@ public class JustDireBlockStates extends BlockStateProvider {
         simpleBlock(Registration.EclipseAlloyBlock.get(), models().cubeAll(Registration.EclipseAlloyBlock_ITEM.getId().getPath(), blockTexture(Registration.EclipseAlloyBlock.get())));
         //simpleBlock(Registration.RawEclipseAlloyOre.get(), models().cubeAll(Registration.RawEclipseAlloyOre_ITEM.getId().getPath(), blockTexture(Registration.RawEclipseAlloyOre.get())).renderType("translucent"));
 
-        simpleBlock(Registration.BlockBreakerT2.get(), models().cubeAll(Registration.BlockBreakerT2_ITEM.getId().getPath(), blockTexture(Registration.BlockBreakerT2.get())));
-        simpleBlock(Registration.BlockPlacerT2.get(), models().cubeAll(Registration.BlockPlacerT2_ITEM.getId().getPath(), blockTexture(Registration.BlockPlacerT2.get())));
-        simpleBlock(Registration.ClickerT2.get(), models().cubeAll(Registration.ClickerT2_ITEM.getId().getPath(), blockTexture(Registration.ClickerT2.get())));
-        simpleBlock(Registration.SensorT2.get(), models().cubeAll(Registration.SensorT2_ITEM.getId().getPath(), blockTexture(Registration.SensorT2.get())));
+        //simpleBlock(Registration.BlockBreakerT2.get(), models().cubeAll(Registration.BlockBreakerT2_ITEM.getId().getPath(), blockTexture(Registration.BlockBreakerT2.get())));
+        //simpleBlock(Registration.BlockPlacerT2.get(), models().cubeAll(Registration.BlockPlacerT2_ITEM.getId().getPath(), blockTexture(Registration.BlockPlacerT2.get())));
+        //simpleBlock(Registration.ClickerT2.get(), models().cubeAll(Registration.ClickerT2_ITEM.getId().getPath(), blockTexture(Registration.ClickerT2.get())));
+        //simpleBlock(Registration.SensorT2.get(), models().cubeAll(Registration.SensorT2_ITEM.getId().getPath(), blockTexture(Registration.SensorT2.get())));
 
         patternBlock();
         soilBlocks();
