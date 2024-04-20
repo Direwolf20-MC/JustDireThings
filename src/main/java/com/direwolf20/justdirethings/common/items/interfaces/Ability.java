dea8bac1c9885fc78cfb1905d4de37bd2ea852c1
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-04-20 12:06:44 -0400

    Set voidshift to Slider ability

--- src/main/java/com/direwolf20/justdirethings/common/items/interfaces/Ability.java
+++ src/main/java/com/direwolf20/justdirethings/common/items/interfaces/Ability.java
@@ -25,7 +25,7 @@ public enum Ability {
     AIRBURST(SettingType.SLIDER, 1, 500),
     //Tier 3
     DROPTELEPORT(SettingType.TOGGLE, 2, 100),
-    VOIDSHIFT(SettingType.TOGGLE, 1, 50), //FE Per block traveled
+    VOIDSHIFT(SettingType.SLIDER, 1, 50), //FE Per block traveled
     //Tier 4
     OREXRAY(SettingType.TOGGLE, 100, 5000),
     GLOWING(SettingType.TOGGLE, 100, 5000),
