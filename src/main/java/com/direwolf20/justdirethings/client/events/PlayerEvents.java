c180e8af320918ab6a13376144465e062bdad235
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-04-23 20:51:31 -0400

    Initial work at rendering extra crumble effects

--- src/main/java/com/direwolf20/justdirethings/client/events/PlayerEvents.java
+++ src/main/java/com/direwolf20/justdirethings/client/events/PlayerEvents.java
@@ -7,10 +7,14 @@ import com.direwolf20.justdirethings.common.items.interfaces.ToggleableTool;
 import com.direwolf20.justdirethings.common.network.data.LeftClickPayload;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.protocol.game.ClientboundBlockDestructionPacket;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.api.distmarker.Dist;
@@ -23,6 +27,8 @@ import java.util.Set;
 
 @Mod.EventBusSubscriber(modid = JustDireThings.MODID, value = Dist.CLIENT)
 public class PlayerEvents {
+    private static BlockPos destroyPos = BlockPos.ZERO;
+    private static int gameTicksMining = 0;
 
     @SubscribeEvent
     public static void LeftClickEmpty(PlayerInteractEvent.LeftClickEmpty event) {
@@ -38,12 +44,68 @@ public class PlayerEvents {
         if (itemStack.getItem() instanceof ToggleableTool toggleableTool && itemStack.getItem() instanceof LeftClickableTool && event.getFace() != null) {
             if (event.getAction() == PlayerInteractEvent.LeftClickBlock.Action.START) { //Only start has the 'proper' direction - this also runs both client AND server side!
                 activateAbilities(itemStack, toggleableTool, event.getEntity(), event.getHand(), false, event.getPos(), event.getFace());
-                //activateAbilities(itemStack, toggleableTool, event.getEntity(), event.getHand(), true, event.getPos(), event.getFace());
-            } else if (event.getAction() == PlayerInteractEvent.LeftClickBlock.Action.ABORT) { //This runs server side only!
-                //activateAbilities(itemStack, toggleableTool, event.getEntity(), event.getHand(), true, event.getPos(), event.getFace());
-                //event.setCanceled(true);
             }
         }
+        if (itemStack.getItem() instanceof ToggleableTool toggleableTool && toggleableTool.hasAbility(Ability.HAMMER) && event.getFace() != null) {
+            doExtraCrumblings(event, itemStack, toggleableTool);
+        }
+    }
+
+    private static void doExtraCrumblings(PlayerInteractEvent.LeftClickBlock event, ItemStack itemStack, ToggleableTool toggleableTool) {
+        Player player = event.getEntity();
+        Level level = player.level();
+        BlockPos blockPos = event.getPos();
+        BlockState blockState = level.getBlockState(blockPos);
+        if (event.getAction() == PlayerInteractEvent.LeftClickBlock.Action.START) { //Client and Server
+            if (level.isClientSide) {
+                gameTicksMining = 0;
+                destroyPos = blockPos;
+            }
+        }
+        if (event.getAction() == PlayerInteractEvent.LeftClickBlock.Action.ABORT) { //Server Only
+            cancelBreaks(level, blockState, blockPos, player, toggleableTool, itemStack);
+        }
+        if (event.getAction() == PlayerInteractEvent.LeftClickBlock.Action.CLIENT_HOLD) { //Client Only
+            if (blockPos.equals(destroyPos)) {
+                gameTicksMining++;
+            } else {
+                gameTicksMining = 0;
+                destroyPos = blockPos;
+            }
+            incrementDestroyProgress(level, blockState, blockPos, player, toggleableTool, itemStack);
+        }
+    }
+
+    private static float incrementDestroyProgress(Level level, BlockState pState, BlockPos pPos, Player player, ToggleableTool toggleableTool, ItemStack toggleableToolStack) {
+        Set<BlockPos> breakBlockPositions = toggleableTool.getBreakBlockPositions(toggleableToolStack, level, pPos, player, pState);
+        int i = gameTicksMining;
+        float f = pState.getDestroyProgress(player, player.level(), pPos) * (float) (i + 1);
+        int j = (int) (f * 10.0F);
+        for (BlockPos blockPos : breakBlockPositions) {
+            System.out.println(generatePosHash(blockPos));
+            if (blockPos.equals(pPos)) continue; //Let the vanilla mechanics handle the block we're hitting
+            if (level.isClientSide)
+                level.destroyBlockProgress(player.getId() + generatePosHash(blockPos), blockPos, j);
+            else
+                sendDestroyBlockProgress(player.getId() + generatePosHash(blockPos), blockPos, -1, (ServerPlayer) player);
+        }
+        return f;
+    }
+
+    private static void cancelBreaks(Level level, BlockState pState, BlockPos pPos, Player player, ToggleableTool toggleableTool, ItemStack toggleableToolStack) {
+        Set<BlockPos> breakBlockPositions = toggleableTool.getBreakBlockPositions(toggleableToolStack, level, pPos, player, pState);
+        for (BlockPos blockPos : breakBlockPositions) {
+            if (blockPos.equals(pPos)) continue; //Let the vanilla mechanics handle the block we're hitting
+            player.level().destroyBlockProgress(player.getId() + generatePosHash(blockPos), blockPos, -1);
+        }
+    }
+
+    public static int generatePosHash(BlockPos blockPos) {
+        return (31 * 31 * blockPos.getX()) + (31 * blockPos.getY()) + blockPos.getZ(); //For now this is probably good enough, will add more randomness if needed
+    }
+
+    public static void sendDestroyBlockProgress(int pBreakerId, BlockPos pPos, int pProgress, ServerPlayer serverPlayer) {
+        serverPlayer.connection.send(new ClientboundBlockDestructionPacket(pBreakerId, pPos, pProgress));
     }
 
     private static void activateAbilities(ItemStack itemStack, ToggleableTool toggleableTool, Player player, InteractionHand hand, boolean air, BlockPos blockPos, Direction direction) {
