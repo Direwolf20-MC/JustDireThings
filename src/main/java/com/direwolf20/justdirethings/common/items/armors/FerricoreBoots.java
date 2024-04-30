c256c8f98e134a6b6df7ea881d3160367b5fb20d
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-04-30 16:02:16 -0400

    Durability returns to armor

--- src/main/java/com/direwolf20/justdirethings/common/items/armors/FerricoreBoots.java
+++ src/main/java/com/direwolf20/justdirethings/common/items/armors/FerricoreBoots.java
@@ -4,11 +4,13 @@ import com.direwolf20.justdirethings.common.items.armors.basearmors.BaseBoots;
 import com.direwolf20.justdirethings.common.items.armors.utils.ArmorTiers;
 import com.direwolf20.justdirethings.common.items.interfaces.Ability;
 import com.direwolf20.justdirethings.common.items.interfaces.AbilityParams;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 
 public class FerricoreBoots extends BaseBoots {
     public FerricoreBoots() {
-        super(ArmorTiers.FERRICORE, new Item.Properties());
+        super(ArmorTiers.FERRICORE, new Item.Properties()
+                .durability(ArmorItem.Type.BOOTS.getDurability(15)));
         registerAbility(Ability.STEPHEIGHT);
         registerAbility(Ability.JUMPBOOST, new AbilityParams(1, 1, 1, 1));
     }
--- src/main/java/com/direwolf20/justdirethings/common/items/armors/FerricoreHelmet.java
+++ src/main/java/com/direwolf20/justdirethings/common/items/armors/FerricoreHelmet.java
@@ -3,10 +3,12 @@ package com.direwolf20.justdirethings.common.items.armors;
 import com.direwolf20.justdirethings.common.items.armors.basearmors.BaseHelmet;
 import com.direwolf20.justdirethings.common.items.armors.utils.ArmorTiers;
 import com.direwolf20.justdirethings.common.items.interfaces.Ability;
+import net.minecraft.world.item.ArmorItem;
 
 public class FerricoreHelmet extends BaseHelmet {
     public FerricoreHelmet() {
-        super(ArmorTiers.FERRICORE, new Properties());
+        super(ArmorTiers.FERRICORE, new Properties()
+                .durability(ArmorItem.Type.HELMET.getDurability(15)));
         registerAbility(Ability.MINDFOG);
     }
 }
