b9ccca4a7f9fd5d24ed8f07935ccbf16c185c468
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-05-03 13:49:07 -0400

    Filter slots now save automatically

--- src/main/java/com/direwolf20/justdirethings/common/network/handler/GhostSlotPacket.java
+++ src/main/java/com/direwolf20/justdirethings/common/network/handler/GhostSlotPacket.java
@@ -1,5 +1,7 @@
 package com.direwolf20.justdirethings.common.network.handler;
 
+import com.direwolf20.justdirethings.common.blockentities.basebe.BaseMachineBE;
+import com.direwolf20.justdirethings.common.containers.basecontainers.BaseMachineContainer;
 import com.direwolf20.justdirethings.common.containers.slots.FilterBasicSlot;
 import com.direwolf20.justdirethings.common.network.data.GhostSlotPayload;
 import net.minecraft.world.entity.player.Player;
@@ -28,6 +30,9 @@ public class GhostSlotPacket {
             stack.setCount(payload.count());
             if (slot instanceof FilterBasicSlot)
                 slot.set(stack);
+
+            if (container instanceof BaseMachineContainer baseMachineContainer && baseMachineContainer.baseMachineBE instanceof BaseMachineBE baseMachineBE)
+                baseMachineBE.markDirtyClient();
         });
     }
 }
