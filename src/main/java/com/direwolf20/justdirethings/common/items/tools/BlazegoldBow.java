0dabf23d7e4c8b50d8994bbb53b6b290bfb60b9a
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-07-16 09:27:42 -0400

    Lava repair on Blazegold Bow

--- src/main/java/com/direwolf20/justdirethings/common/items/tools/BlazegoldBow.java
+++ src/main/java/com/direwolf20/justdirethings/common/items/tools/BlazegoldBow.java
@@ -1,16 +1,26 @@
 package com.direwolf20.justdirethings.common.items.tools;
 
 import com.direwolf20.justdirethings.common.items.interfaces.Ability;
+import com.direwolf20.justdirethings.common.items.interfaces.Helpers;
 import com.direwolf20.justdirethings.common.items.tools.basetools.BaseBow;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
 
 public class BlazegoldBow extends BaseBow {
     public BlazegoldBow() {
-        super(new Properties().durability(450));
+        super(new Properties().durability(450).fireResistant());
         registerAbility(Ability.SPLASH);
         registerAbility(Ability.LAVAREPAIR);
     }
 
     public float getMaxDraw() {
         return 15;
+    }
+
+    @Override
+    public boolean onEntityItemUpdate(ItemStack stack, ItemEntity entity) {
+        if (canUseAbility(stack, Ability.LAVAREPAIR))
+            return Helpers.doLavaRepair(stack, entity);
+        return false;
     }
 }
