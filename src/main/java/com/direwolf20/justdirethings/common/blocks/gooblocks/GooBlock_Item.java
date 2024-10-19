7974e4a19c2c2e101f2857a993873641c9db0c25
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2024-10-18 21:17:27 -0400

    When CAN_GOO_DIE is set to false, placed goo is alive by default. Resolves #208

--- src/main/java/com/direwolf20/justdirethings/common/blocks/gooblocks/GooBlock_Item.java
+++ src/main/java/com/direwolf20/justdirethings/common/blocks/gooblocks/GooBlock_Item.java
@@ -1,13 +1,16 @@
 package com.direwolf20.justdirethings.common.blocks.gooblocks;
 
+import com.direwolf20.justdirethings.setup.Config;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.List;
 
@@ -26,5 +29,13 @@ public class GooBlock_Item extends BlockItem {
 
         tooltip.add(Component.translatable("justdirethings.requiresfeeding")
                 .withStyle(ChatFormatting.DARK_RED));
+    }
+
+    @Override
+    protected BlockState getPlacementState(BlockPlaceContext context) {
+        BlockState blockstate = Config.GOO_CAN_DIE.get() ? this.getBlock().getStateForPlacement(context) : this.getBlock().getStateForPlacement(context).setValue(GooBlock_Base.ALIVE, true);
+        if (blockstate == null)
+            return null;
+        return this.canPlace(context, blockstate) ? blockstate : null;
     }
 }
